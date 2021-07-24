09ea33e548246225a94154d9a064e303cc018018
Mahesh H P <hpmahesh73@gmail.com>
2021-07-24 11:16:55 +0530

    fix: analytics data fn execution error (#1)
    * temp: add console error
    
    * fix: fn data parsing
    
    * fix: execution error

--- functions/analyticsData.js
+++ functions/analyticsData.js
@@ -23,12 +23,18 @@ exports.handler = async (event) => {
         const formattedData = {
             visits: analyticsData.length,
             pageWiseVisits: analyticsData.reduce((acc, value) => {
+                const visitTimeData = value.visit_time ? {
+                    time: value.visit_time.time,
+                    tz: value.visit_time.tz
+                } : {
+                    time: value.visit_time,
+                    tz: null
+                };
                 const formattedValue = {
                     page: value.page,
                     userAgent: value.user_agent,
-                    visitData: {
-                        time: value.visit_time.time || value.visit_time,
-                        tz: value.visit_time.tz || null
+                    visitTime: {
+                        ...visitTimeData
                     },
                     logTime: value.log_time
                 }
